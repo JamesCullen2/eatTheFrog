cecc1873d0e821264af8f85378b263b4b51fbe8c
JamesCullen2 <jamesnac@aol.com>
2018-07-26 14:14:19 +0100

    constructing of todo list components

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    <todo-list></todo-list>
+  <div id="app">
+    <h1 class="ui dividing centered header">Vue.js Todo App</h1>
+    <div class='ui three column centered grid'>
+      <div class='column'>
+        <todo-list v-bind:todos="todos"></todo-list>
+        <create-todo v-on:create-todo="createTodo"></create-todo>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,6 +18,34 @@ export default {
   components: {
     TodoList,
   },
+
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+          title: 'Todo B',
+          project: 'Project B',
+          done: true,
+      }, {
+          title: 'Todo C',
+          project: 'Project C',
+          done: false,
+      }, {
+          title: 'Todo D',
+          project: 'Project D',
+          done: false,
+      }],
+    };
+  },
+  methods: {
+      createTodo(newTodo) {
+        this.todos.push(newTodo);
+        sweetalert('Success!', 'To-Do created!', 'success');
+      },
+    },
 };
 </script>
 
